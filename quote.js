6f6de3a5e8b2cc30bbf8cfc560c85f1c4fa0bc40
SAHIL KATOCH <80945456+sahil290@users.noreply.github.com>
2021-08-08 21:22:27 +0530

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,56 @@
+
+// Collection Of Quote
+const quotes = [{
+    quote: `I'm not a great programmer; I'm just a good programmer with great habits.`,
+    author: `â€• Kent Beck`
+}, {
+    quote: `Talk is cheap. Show me the code.`,
+    author: `â€• Linus Torvalds`
+}, {
+    quote: `Programs must be written for people to read, and only incidentally for machines to execute.`,
+    author: `â€• Harold Abelson`
+}, {
+    quote: `Truth can only be found in one place: the code.`,
+    author: `â€• Robert C`
+}, {
+    quote: `Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime.`,
+    author: `â€• Muhammad Waseem`
+}, {
+    quote: `How you look at it is pretty much how you'll see it`,
+    author: `â€• Steve Jobs`
+}, {
+    quote: `The most disastrous thing that you can ever learn is your first programming language.`,
+    author: `â€• Alan Kay`
+}, {
+    quote: `The most important property of a program is whether it accomplishes the intention of its user.`,
+    author: `â€• C.A.R. Hoare`
+}, {
+    quote: `i am committed to push my branch to the master.`,
+    author: `â€• Halgurd Hussein`
+}, {
+    quote: `Coding is not just code, that is a live thing to serve everyone!`,
+    author: `â€• Ming Song`
+},]
+// New Quote Button
+const quoteBtn = document.getElementById('quote-btn');
+const changeColor = document.getElementById('quote-btn'),
+    colors = ['red', 'green', 'blue', 'grey', 'white', 'black', 'darkblue', 'darkOrange'];
+let colorIndex = 0;
+changeColor.addEventListener('click', () => {
+    document.body.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+});
+/* BUTTON BACKGROUND COLOR CHANGING CODE */
+document.getElementById("quote-btn").onclick = function () {
+    document.getElementById("quote-btn").style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+}
+// Get Quote and author Section
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+// Event Handler
+quoteBtn.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * quotes.length);
+    quote.innerHTML = quotes[random].quote;
+    author.innerHTML = quotes[random].author;
+})
